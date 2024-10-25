29dd76f7180ac5ef4e8e6d2eed4566dc9ec4dba3
Leo <agustineleo@pm.me>
2024-10-26 00:23:11 +0800

    update lspconfig +gopls

--- config/nvim/lua/plugins/lsp_config.lua
+++ config/nvim/lua/plugins/lsp_config.lua
@@ -230,6 +230,9 @@ return {
 				},
 			}
 
+			local lspconfig = require("lspconfig")
+			lspconfig.gopls.setup({})
+
 			-- Ensure the servers and tools above are installed
 			--  To check the current status of installed tools and/or manually install
 			--  other tools, you can run
